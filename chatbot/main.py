fc718235dfe62b78451996184c8f22c9027ad805
Tae Kim <taekim0508@gmail.com>
2025-09-03 22:56:50 -0500

    Delete old chatbot file

--- chatbot/main.py
+++
@@ -1,9 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key = "AIzaSyBrzBMgFnn_mSw8Us_0sxPHwGegwJe9fqY")
-response = client.models.generate_content(
-    model = "gemma-3-27b-it",
-    contents = "Explain what a LLM is in three sentences."
-)
-
-print(response.text)
